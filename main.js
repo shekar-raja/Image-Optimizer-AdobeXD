f3a70e20b3784fabb3a6c8b8335f215612417d70
Rajasekhar Balamurugan <rajasekhar.b@tadigital.com>
2018-10-26 13:50:49 +0530

    For two iterations it works

--- main.js
+++ main.js
@@ -6,92 +6,102 @@ const command = require("commands");
 // Main Handler Function
 async function myPluginCommand(selection) 
 {   
-    const pluginFolder = await fs.getTemporaryFolder()    
+    const pluginFolder = await fs.getTemporaryFolder();
+    // Creating temporary file
+    const file = await pluginFolder.createFile('compressed.jpg', { overwrite: true });
+        
+    // Creating renditions
+    const renditions = [{
+        node: selection.items[0],
+        outputFile: file,
+        type: "jpg",
+        scale: 1.0,
+        quality:50
+    }];
 
+    // User should select atleast one node.
     if (selection.items.length > 0) 
     {
-        // Creating temporary file
-        const file = await pluginFolder.createFile('compressed.jpg', { overwrite: true });
-        
-        // Creating renditions
-        const renditions = [{
-            node: selection.items[0],
-            outputFile: file,
-            type: "jpg",
-            scale: 1.0,
-            quality:50
-        }];
-        try{
-        const imageentry = await pluginFolder.getEntry("compressed.jpg");
-        }catch(Error)
-        {
-            console.log(Error);
-        }
-        try{
-            const imageentry = await pluginFolder.getEntry("compressed.jpg");
-        if(imageentry.name == "compressed.jpg"){
+        // for(var i=1; i < 4;i++)
+        // {
+            // if( i === 1 )
+            // {
+                // Creating rendition
+                application.createRenditions(renditions)
+                    .then(results => {
+                        
+                        // // create the dialog
+                        // let dialog = document.createElement("dialog");
 
-            await imageentry.delete();
-            console.log("Deleted temporary storage!");
-        }
-        else
-        {
-            console.log("No file found");
-        }
-        }catch(Error)
-        {
-            console.log(Error);
-        }
-        application.createRenditions(renditions)
-            .then(results => {
-                
-                // // create the dialog
-                // let dialog = document.createElement("dialog");
+                        // // main container
+                        // let container = document.createElement("div");
+                        // container.style.minWidth = 400;
+                        // container.style.padding = 60;
 
-                // // main container
-                // let container = document.createElement("div");
-                // container.style.minWidth = 400;
-                // container.style.padding = 60;
+                        // // add conten
+                        // let title = document.createElement("h2");
+                        // title.style.padding = 20;
+                        // title.textContent = `Compress Now worked!`;
+                        // container.appendChild(title);
 
-                // // add conten
-                // let title = document.createElement("h2");
-                // title.style.padding = 20;
-                // title.textContent = `Compress Now worked!`;
-                // container.appendChild(title);
+                        // // close button
+                        // let closeButton = document.createElement("button");
+                        // closeButton.textContent = "Got it!";
+                        // closeButton.style.padding = 20;
+                        // container.appendChild(closeButton);
+                        // closeButton.onclick = (e) => {
+                        //     dialog.close();
+                        // }
 
-                // // close button
-                // let closeButton = document.createElement("button");
-                // closeButton.textContent = "Got it!";
-                // closeButton.style.padding = 20;
-                // container.appendChild(closeButton);
-                // closeButton.onclick = (e) => {
-                //     dialog.close();
-                // }
+                        // document.body.appendChild(dialog);
+                        // dialog.appendChild(container);
+                        // dialog.showModal()
+                        console.log("Image Created");
+                        
+                    })
+                    .catch(error => {
+                    })
+            // }   
+            
+            // else if( i === 2)
+            // {
+                let fill = new ImageFill(file);
+                selection.items[0].fill = fill;
+            // }
+            // else if(i ===2 )
+            // {
+                // Checking the entry
+                try
+                {
+                    const imageentry = await pluginFolder.getEntry("compressed.jpg");
+                    console.log("Found compressed image entry");
+                }
+                catch(Error)
+                {
+                    console.log("No compressed image entry found");
+                }
 
-                // document.body.appendChild(dialog);
-                // dialog.appendChild(container);
-                // dialog.showModal()
-                console.log("Image Created");
-                
-            })
-            .catch(error => {
-                
-            })
-        
-        let fill = new ImageFill(file);
-        selection.items[0].fill = fil;
-        // filler(file,selection.items[0])
-        
-        // Deleting the temporarily stored image.
-        
+                // Deleting temporary file
+                try
+                {
+                    const imageentry = await pluginFolder.getEntry("compressed.jpg");
+                    if(imageentry.name == "compressed.jpg")
+                    {
+                        await imageentry.delete();
+                        console.log("Deleted temporary storage!");
+                    }
+                    else
+                    {
+                        console.log("No file found");
+                    }
+                }
+                catch(Error)
+                {
+                    console.log("Can't delete image");
+                }
+            // }   
+        // }     
     }
-}
-
-function filler(file,node)
-{
-    let fill = new ImageFill(file);
-
-    node.fill = fill;
 }
 
 // Exporting modules via JSON.
