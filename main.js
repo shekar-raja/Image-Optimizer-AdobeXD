85b8cd6c95ebcd41cc5d931fcfdea902ef0e813e
Rajasekhar Balamurugan <rajasekhar.b@tadigital.com>
2018-10-26 21:02:43 +0530

    Added alert and error messages in UI

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ const application = require("application");
 const fs = require("uxp").storage.localFileSystem;
 const ImageFill = require("scenegraph").ImageFill;
 const command = require("commands");
-
+const { alert, error } = require("./lib/dialogs.js");
 // Iterating two times for making rendered image into fill.
 for(let i=0;i<2;i++)
 {
 // Main Handler Function
 async function mainFunction(selection) 
 {   
-        //     
+        // Temorary folder 
         const tempFolder = await fs.getTemporaryFolder();
         // Creating temporary file
         const file = await tempFolder.createFile('compressed.jpg', { overwrite: true });
@@ -23,7 +23,7 @@ async function mainFunction(selection)
             scale: 1.0,
             quality:60
         }];
-    
+        
         // User should select atleast one node.
         if (selection.items.length > 0) 
         {
@@ -70,10 +70,13 @@ async function mainFunction(selection)
                 let fill = new ImageFill(file);
                 
                 selection.items[0].fill = fill;
+                
+                showAlert();
             }
             catch(Error)
             {
-                console.log("Image not filled")
+                console.log("Image not compressed and filled")
+                showError();
             }    
 
             // Try block for checking if there image entry
@@ -89,22 +92,36 @@ async function mainFunction(selection)
                 else
                 {
                     // UI to print Select image file to compress
+                    showError();
                 }
             }
             catch(Error)
             {
-                console.log("Can't delete image");
+                showError();
+                // console.log("Can't delete image");
             }
                 
         }
         else
         {
-            // UI for printing Select an image file to perform compression.
+            error();
         }
     
     
 }
 
+// Function for showing alert
+async function showAlert()
+{
+    await alert("Image Compression Successful!");
+}
+
+// Asynchronous Function to show error
+async function showError()
+{
+    await error("Error in compression",
+                "Please select a image file to perform compression");
+}
 
 // Exporting modules via JSON.
 module.exports = {
